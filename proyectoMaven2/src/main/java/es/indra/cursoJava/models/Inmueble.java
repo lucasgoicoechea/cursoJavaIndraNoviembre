dda30c5e1b811f5783ad71b2872955df37ed29d3
ddominguezy <ddominguezy@indra.es>
2022-11-23 16:48:34 +0100

    LOMBOK-DanielDominguez-JavierTrigos

--- proyectoMaven2/src/main/java/es/indra/cursoJava/models/Inmueble.java
+++ proyectoMaven2/src/main/java/es/indra/cursoJava/models/Inmueble.java
@@ -1,9 +1,13 @@
 package es.indra.cursoJava.models;
 
 import es.indra.cursoJava.interfaces.IInmueble;
+import lombok.Builder;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
 @Data
+@Builder
+@Slf4j
 public class Inmueble implements IInmueble {
 
 	protected Inventario unInventario;
--- proyectoMaven2/src/main/java/es/indra/cursoJava/models/Inventario.java
+++ proyectoMaven2/src/main/java/es/indra/cursoJava/models/Inventario.java
@@ -1,5 +1,12 @@
 package es.indra.cursoJava.models;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@Data
+@Builder
+@Slf4j
 public class Inventario {
 
 }
