b1ca12a55851e7b2d77ab868ede289b71cc35516
mtaboas <118349440+mtaboas@users.noreply.github.com>
2022-11-28 11:52:18 +0100

    Delete Proyectos ejemplo directory

--- Proyectos ejemplo/ExceptionsEjemploV1/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author usuario
- *
- */
-module ExceptionsEjemploV1 {
-}
--- Proyectos ejemplo/com/indra/demo/PersonaRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.indra.demo;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonaRepositorio extends CrudRepository<Persona,Integer> {
-
-}
