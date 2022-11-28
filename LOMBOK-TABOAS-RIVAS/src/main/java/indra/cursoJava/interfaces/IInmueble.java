0d4eea13fa0682b3568e7054151ae62a77ab5daa
mtaboas <118349440+mtaboas@users.noreply.github.com>
2022-11-28 11:51:32 +0100

    Delete LOMBOK-TABOAS-RIVAS directory

--- LOMBOK-TABOAS-RIVAS/src/main/java/indra/cursoJava/interfaces/IInmueble.java
+++
@@ -1,9 +0,0 @@
-package indra.cursoJava.interfaces;
-
-import indra.cursoJava.models.Inventario;
-
-public interface IInmueble {
-	public String getDirection();
-	public Inventario getInventario();
-	
-}
--- LOMBOK-TABOAS-RIVAS/src/main/java/indra/cursoJava/models/Inventario.java
+++
@@ -1,8 +0,0 @@
-package indra.cursoJava.models;
-
-import lombok.Data;
-
-@Data
-public class Inventario {
-
-}
