054361629554bbbda5a9a9a8529c88fb8c901f1d
emfernandezp <emfernandezp@indra.es>
2022-11-30 14:01:40 +0100

    cambiado el word

--- SpringMVCCrud/src/main/java/com/springmvc/model/Houston.java
+++ SpringMVCCrud/src/main/java/com/springmvc/model/Houston.java
@@ -1,6 +1,14 @@
 package com.springmvc.model;
 
+import java.util.ArrayList;
+
 public class Houston {
 
 	private int id;
+	private String nombre;
+	private int capacidad;
+	private int num_lanz;
+	private ArrayList<Rocket> lanzamientos;
+	
+	
 }
