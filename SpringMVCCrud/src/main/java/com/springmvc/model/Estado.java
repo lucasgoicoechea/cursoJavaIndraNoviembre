39131c0ab1bc2c39bc50b3ce938595c01c304089
jesusffuncia <118349571+jesusffuncia@users.noreply.github.com>
2022-11-30 16:07:45 +0100

    Desarrollo de los requisitos fundamentales

---
+++ SpringMVCCrud/src/main/java/com/springmvc/model/Estado.java
@@ -0,0 +1,5 @@
+package com.springmvc.model;
+
+public enum Estado {
+	DESARROLLANDO, ESPERANDO, LANZADO;
+}
--- SpringMVCCrud/src/main/java/com/springmvc/model/Rocket.java
+++ SpringMVCCrud/src/main/java/com/springmvc/model/Rocket.java
@@ -1,5 +1,12 @@
 package com.springmvc.model;
 
+import java.util.Date;
+
 public class Rocket {
+	private Date Fecha_lanzamiento;
+	private String Nombre;
+	private int id;
+	private int id_base;
+	private Estado Estado_mision;
 
 }
