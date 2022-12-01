00509b1e1e55eceebbb10c68d36f442271c150d2
emfernandezp <emfernandezp@indra.es>
2022-12-01 12:26:47 +0100

    Agregados metodos a clase Houston

--- SpringMVCHibernate/target/generated-sources/annotations/springmvc/Houston.java
+++ SpringMVCHibernate/target/generated-sources/annotations/springmvc/Houston.java
@@ -11,6 +11,32 @@ public class Houston {
 	private int capacidad;
 	private int num_lanz;
 	private ArrayList<Rocket> lanzamientos;
-	
-	
+
+	public void cohe_lanz(ArrayList<Rocket> lanz,int id) {
+		for (Rocket rocket : lanz) {
+			if(rocket.getId()==id)rocket.cambiar_estado(Estado.LANZADO);
+		}
+	}
+
+	public void Add_rocket(Rocket cohete) {
+		this.lanzamientos.add(cohete);
+	}
+
+	public void Remove_rocket(Rocket cohete) {
+		this.lanzamientos.remove(cohete);
+	}
+
+	public int N_lanzamientos(int Num_lanz) {
+		return Num_lanz;
+	}
+
+	public int N_esperando(ArrayList<Rocket> lanzamientos) {
+		int numCola = 0;
+		for (Rocket rocket : lanzamientos) {
+			if(rocket.getEstado()==Estado.ESPERANDO)numCola++;
+		}
+		return numCola;
+
+	}
+
 }
