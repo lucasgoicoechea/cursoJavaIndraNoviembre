a90d8d503e7cecae7646e2cb0e631255565e3393
ddominguezy <ddominguezy@indra.es>
2022-12-15 09:34:39 +0100

    Subido proyecto ejemplo de aspectos con SpringBoot

---
+++ AspectosEjemplo/src/main/java/es/indra/AspectoTiempo.java
@@ -0,0 +1,31 @@
+package es.indra;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class AspectoTiempo {
+
+	@Around("execution(* es.indra.*.*(..))")
+	public Object calculoTiempo(ProceedingJoinPoint joinPoint) throws Throwable {
+
+		long t1 = System.currentTimeMillis();
+
+		Object resultado = joinPoint.proceed();
+
+		long t2 = System.currentTimeMillis();
+
+		if (t2 - t1 > 2000) {
+
+			System.out.println("Metodo lento:" + joinPoint.getTarget().getClass() + "."
+					+ joinPoint.getSignature().getName() + ":" + (t2 - t1));
+		}
+
+		return resultado;
+
+	}
+
+}
---
+++ AspectosEjemplo/src/test/java/es/indra/AspectosEjemploApplicationTests.java
@@ -0,0 +1,13 @@
+package es.indra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AspectosEjemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
